294ba1ea622f08617b27ecc3afd32051750f09c5
Bobby Uno <robert.y.uno@gmail.com>
2020-04-02 00:29:34 +0000

    authentication added

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,11 @@
 class Admin::DashboardController < ApplicationController
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
+  
+  # , except: [:index, :show]
+ 
+  def index
+    @articles = Article.all
+  end
   def show
   end
 end
