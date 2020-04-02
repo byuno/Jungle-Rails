1276af3d39742ed0f41a9ffbd38bcbe733367219
Bobby Uno <robert.y.uno@gmail.com>
2020-04-02 20:08:58 +0000

    fully implimented admin categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,16 +1,29 @@
 class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
 
   def index
-    @categories = Category.all
+    @categories = Category.order(created_at: :desc).all
   end
 
   def new
-    @categories = Category.new
+    @category = Category.new
   end
 
   def create
-    
+    @category = Category.new(product_params)
+    if @category.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
   end
 
+end
 
+def product_params
+  params.require(:category).permit(
+    :name,
+    :created_at,
+    :updated_at
+  )
 end
