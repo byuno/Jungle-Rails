37e7e12c438538eec03d3620e269b7d174cd3983
Bobby Uno <robert.y.uno@gmail.com>
2020-04-07 00:57:21 +0000

    added all validations

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -2,19 +2,42 @@ require 'rails_helper'
 
 RSpec.describe Product, type: :model do
   describe 'Validations' do
-    subject{
-      @category = Category.new(:name => "Funiture"),
-      @product = Product.new(:name => "Chair", :price => 20, :quantity => 2, :category => @category)
+    
+    subject {
+      described_class.new(:name => "Chair", :price_cents => 20, :price => 30, :quantity => 2, :category => Category.new(:name => "neato"))
     }
 
     it "is valid with valid attributes" do
-      
       expect(subject).to be_valid
     end
 
-    it "is not valid without a name"
-    it "is not valid without a price"
-    it "is not valid without a quantity"
-    it "is not valid without a category"
+    it "is not valid without a name" do
+      subject.name = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a price" do
+      subject.price_cents = nil
+      expect(subject).to_not be_valid
+    end
+
+    # it "is not valid without a price" do
+    #   subject.price = nil
+    #   expect(subject).to_not be_valid
+    # end
+
+    it "is not valid without a quantity" do
+      subject.quantity = nil
+      expect(subject).to_not be_valid
+    end
+
+    it "is not valid without a category" do
+      subject.category = nil
+      expect(subject).to_not be_valid
+    end
+
   end
 end
+
+
+
