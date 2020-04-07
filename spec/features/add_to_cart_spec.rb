b9edc9c95e2c9c1fab50ff3cf6686f156ffc0327
Bobby Uno <robert.y.uno@gmail.com>
2020-04-07 21:36:15 +0000

    add to cart test

---
+++ spec/features/add_to_cart_spec.rb
@@ -0,0 +1,31 @@
+require 'rails_helper'
+
+RSpec.feature "Visitor adds to cart", type: :feature, js: true do
+
+  # SETUP
+  before :each do
+    @category = Category.create! name: 'Apparel'
+
+    @category.products.create!(name: "Chair",
+    description: "It's wooden",
+    image: open_asset('apparel1.jpg'),
+    quantity: 100000,
+    price: 9.99)
+
+  end
+
+  scenario "They see cart amount go up by 1" do
+    visit root_path
+    click_button 'Add'
+    click_button 'Add'
+    click_button 'Add'
+
+
+
+    # commented out b/c it's for debugging only
+    save_and_open_screenshot
+
+    expect(page).to have_content("(1)")
+    puts page.html
+  end
+end
